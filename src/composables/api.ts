79a3d9ce244f029a6adb0dbe033551fa324788df
Raul Domingues <raaaaul@live.com>
2022-06-19 00:28:17 -0300

    start api composable interface: '

---
+++ src/composables/api.ts
@@ -0,0 +1,37 @@
+// these APIs are auto-imported from @vueuse/core
+import { useCacheStore } from '~/store/cache'
+import type { setCacheConfig } from '~/store/cache'
+
+const defaultConfig = {
+  baseUrl: import.meta.env.BASE_URL,
+}
+// USE CHACHE STORE
+const useCache = useCacheStore()
+
+/**
+ * (Async) (queryString)
+ * Send GET request to API
+ * Return all available records
+ */
+export async function useFind(url: string, config?: setCacheConfig) {
+  // CREATE HASH
+  const hash = useCache.createHash(`find__${url}`)
+  // CHECK IF CACHED
+  const cached = useCache.getValue(hash)
+  // RETURN CACHED DATA
+  if (cached)
+    return cached
+
+  // FETCH API
+  const { error, data, response } = await useFetch(url, {})
+
+  if (response.value && response.value?.status >= 200 && response.value?.status < 300) {
+    // CACHE RESPONSE IF CONFIG IS SET
+    if (config && config.cache)
+      useCache.setValue(hash, data, { ...config })
+  }
+  else {
+    // RETURN ERROR
+    return error
+  }
+}
