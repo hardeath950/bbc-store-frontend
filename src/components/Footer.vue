1f95bffe466422c289c3ee97c2e129fd0a66d383
Raul Domingues <raaaaul@live.com>
2022-06-25 20:19:19 -0300

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,7 +26,7 @@ const toggleLocales = () => {
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2"  rel="noopener" href="https://github.com/hardeath950/bbc-store-frontend" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
