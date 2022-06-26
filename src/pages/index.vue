8ab920ae5d04ee2922d03c42aa319c263051d50f
Raul Domingues <raaaaul@live.com>
2022-06-26 17:10:07 -0300

    first test with api

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { useFind } from '~/composables/api'
-const user = useUserStore()
-const input = $ref(user.savedName)
 
 interface Product {
   id: number
@@ -21,12 +19,6 @@ async function loadProducts() {
   products.value = content.value.data
 }
 
-const router = useRouter()
-const go = () => {
-  if (input)
-    router.push(`/hi/${encodeURIComponent(input)}`)
-}
-
 const { t } = useI18n()
 
 loadProducts()
@@ -35,7 +27,7 @@ loadProducts()
 <template>
   <div>
     <div text-4xl>
-      <div i-carbon-campsite inline-block />
+      <div i-carbon-store inline-block />
     </div>
     <p>
       <a rel="noopener" href="https://github.com/hardeath950/bbc-store-frontend" target="_blank">
@@ -45,38 +37,9 @@ loadProducts()
     <p>
       <em text-sm opacity-75>{{ t('intro.desc') }}</em>
     </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="input"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x4 y2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!input"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
   </div>
   <!-- PRODUCTS WRAPPER -->
-  <div class="flex items-center justify-center">
+  <div class="my-6 flex items-center justify-center space-x-4">
     <!-- PRODUCT -->
     <div
       v-for="product, index in products"
